1d2085d0650e9c112714fea9ffdcf37e4407539e
Luzyanin Kirill <jk87@rambler.ru>
2023-06-01 00:07:34 +0200

    City project

---
+++ src/main/java/de/ait/city/entity/City.java
@@ -0,0 +1,29 @@
+package de.ait.city.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="town")
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class City {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name = "id", unique = true)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "country")
+    private String country;
+    @Column(name = "population")
+    private int population;
+    @Column(name = "area")
+    private int area;
+
+}
---
+++ src/main/java/de/ait/city/service/CityServiceImpl.java
@@ -0,0 +1,65 @@
+package de.ait.city.service;
+
+import de.ait.city.dto.CityRequestDTO;
+import de.ait.city.dto.CityResponseDTO;
+import de.ait.city.entity.City;
+import de.ait.city.repository.CityRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class CityServiceImpl {
+
+    private CityRepository repository;
+
+    /*
+    public List<City> getAllCities() {
+        List<City> list = new ArrayList<>();
+        //repository.findAll().forEach(l->list.add(l));   ???
+        repository.findAll().forEach(list::add);
+        return list;
+    }
+
+    public City addCity(City city) {
+        repository.save(city);
+        return city;
+    }
+    */
+
+    // с использованием DTO
+    public List<CityResponseDTO> getAllCities() {
+        List<CityResponseDTO> list = new ArrayList<>();
+        repository.findAll().forEach(
+                c -> {
+                    CityResponseDTO cdto = new CityResponseDTO(
+                            c.getId(),
+                            c.getName(),
+                            c.getCountry(),
+                            c.getPopulation(),
+                            c.getArea()
+                    );
+                    list.add(cdto);
+                }
+        );
+        return list;
+    }
+
+    public CityResponseDTO addCity(CityRequestDTO city) {
+        City c = new City(null, city.getName(), city.getCountry(), city.getPopulation(), city.getArea());
+        repository.save(c);
+        CityResponseDTO crdto = new CityResponseDTO();
+        return crdto; //??? return c;?
+    }
+
+    public CityResponseDTO updateCity(Long id, CityRequestDTO city) {
+        City c = new City(id, city.getName(), city.getCountry(), city.getPopulation(), city.getArea());
+        repository.save(c);
+        CityResponseDTO crdto = new CityResponseDTO();
+        return crdto;
+    }
+
+}
